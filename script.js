43cce52a170b1a00ccbc2077411ec5ae83bfbc65
maiconc137 <qumaicon@gmail.com>
2023-07-15 02:01:39 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("confirmarPresenca").addEventListener("click", function() {
+  var alineContainer = document.getElementById("alineContainer");
+  alineContainer.classList.add("show-animation");
+});
